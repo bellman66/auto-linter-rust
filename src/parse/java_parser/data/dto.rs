db8ecc94e28c9cfcff3bbdd9084f5500efb9f729
rndso15 <rndso15@naver.com>
2023-12-30 18:10:06 +0900

    feat: Header 값 제작 로직 추가

--- src/parse/java_parser/data/dto.rs
+++ src/parse/java_parser/data/dto.rs
@@ -1,18 +1,23 @@
-use std::collections::hash_map::Values;
-use crate::parse::java_parser::context::extract_header_line;
-
 
 #[derive(Debug)]
 pub struct Header {
+    is_package_contains: bool,
     value_array: Vec<String>
 }
 
 impl Header {
 
+    const NEXT_LINE: &'static str = "\n";
+
     pub fn create(content: String) -> Self {
         Header {
-            value_array: extract_header_line(content)
+            is_package_contains: Self::check_package_previous(&content),
+            value_array: Self::extract_header_line(content)
         }
+    }
+
+    fn check_package_previous(content: &String) -> bool {
+        content.contains("package ")
     }
 
     fn extract_header_line(content: String) -> Vec<String> {
@@ -22,8 +27,22 @@ impl Header {
             .collect::<Vec<String>>()
     }
 
-    pub fn get_content(&self) -> &Vec<String> {
-        &self.value_array
+    pub fn get_content(&self) -> String {
+        let mut result = String::new();
+        let mut start = 0;
+        let end = self.value_array.len();
+
+        if self.is_package_contains {
+            start = 1;
+            result.push_str(&self.value_array[0]);
+            result.push_str(Self::NEXT_LINE)
+        }
+
+        for idx in start..end {
+            result.push_str(&self.value_array[idx]);
+            result.push_str(Self::NEXT_LINE)
+        }
+        result
     }
 }
 
